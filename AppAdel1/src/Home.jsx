9f66fc59c96a79344b072fc0d5ad97277523cf2a
Joao-Ortega <joao_ortega17@hotmail.com>
2024-05-15 12:30:22 -0300

    Adding instagram link

--- AppAdel1/src/Home.jsx
+++ AppAdel1/src/Home.jsx
@@ -73,11 +73,12 @@ function Home() {
 
           </div>
           <div className="icones">
-
-            <div className="paginstagram">
-              <img src={instagram} alt="Instagram" />
-              <p>Instagram</p>
-            </div>
+            <a target='_blank' rel="stylesheet" href="https://www.instagram.com/adelparquealvoradai/">
+              <div className="paginstagram">
+                <img src={instagram} alt="Instagram" />
+                <p>Instagram</p>
+              </div>
+            </a>
 
             <div className="paggrupo">
               <img src={grupo} alt="PGI" />
