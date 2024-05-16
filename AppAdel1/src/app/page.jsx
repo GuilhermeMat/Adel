b8d013862cc4939a6cee347bef0fedf772e34fa4
Joao-Ortega <joao_ortega17@hotmail.com>
2024-05-16 14:21:48 -0300

    Configuring Next and Next Router

---
+++ AppAdel1/src/app/page.jsx
@@ -0,0 +1,7 @@
+import Home from "../components/Home";
+import '../styles/home.css'
+import '../styles/index.css'
+
+export default function Page() {
+  return <Home />
+}
--- AppAdel1/src/components/Home.jsx
+++ AppAdel1/src/components/Home.jsx
@@ -1,37 +1,39 @@
-import { useState } from 'react'
-import logo from "./img/adel.png"
-import config from "./img/configuracao.png"
-import perfil from "./img/usuario.png"
-import igreja from "./img/igreja.png"
-import ministerio from "./img/ministerio.png"
-import noticia from "./img/noticia.png"
-import biblia from "./img/biblia.png"
-import oracao from "./img/oracao.png"
-import foto from "./img/foto.png"
-import instagram from "./img/instagram.png"
-import grupo from "./img/grupo.png"
-import agenda from "./img/agenda.png"
+'use client'
+import logo from "../img/adel.png"
+import config from "../img/configuracao.png"
+import perfil from "../img/usuario.png"
+import igreja from "../img/igreja.png"
+import ministerio from "../img/ministerio.png"
+import noticia from "../img/noticia.png"
+import biblia from "../img/biblia.png"
+import oracao from "../img/oracao.png"
+import foto from "../img/foto.png"
+import instagram from "../img/instagram.png"
+import grupo from "../img/grupo.png"
+import agenda from "../img/agenda.png"
+import { useRouter } from "next/navigation"
 
 
 function Home() {
-cd 
+  const router = useRouter()
+
   return (
       <div className="capsula">
 
         <div className="nav">
 
           <div className="config">
-            <img src={config} alt="Configuração" className='imgconfig'/>
+            <img src={config.src} alt="Configuração" className='imgconfig'/>
           </div>
 
           <div className="perfil">
-            <img src={perfil} alt="Perfil" className='imgperfil'/>
+            <img src={perfil.src} alt="Perfil" className='imgperfil'/>
           </div>
 
         </div>
 
         <div className='divlogo'>
-          <img src={logo} alt="Adel Parque Alvorada 1" className='imglogo'/>
+          <img src={logo.src} alt="Adel Parque Alvorada 1" className='imglogo'/>
         </div>
 
         <div className="icons">
@@ -39,17 +41,17 @@ cd
           <div className="icones">
 
             <div className="pagigreja">
-              <img src={igreja} alt="Igreja" />
+              <img src={igreja.src} alt="Igreja" />
               <p>Igreja</p>
             </div>
 
             <div className="pagministerio">
-              <img src={ministerio} alt="Ministerio" />
+              <img src={ministerio.src} alt="Ministerio" />
               <p>Ministério</p>
             </div>
 
             <div className="pagnoticia">
-              <img src={noticia} alt="Notícias" />
+              <img src={noticia.src} alt="Notícias" />
               <p>Notícias</p>
             </div>
 
@@ -57,17 +59,17 @@ cd
           <div className="icones">
 
             <div className="pagbiblia">
-              <img src={biblia} alt="Bíblia" />
+              <img src={biblia.src} alt="Bíblia" />
               <p>Bíblia</p>
             </div>
 
             <div className="pagoracao">
-              <img src={oracao} alt="Oração" />
+              <img src={oracao.src} alt="Oração" />
               <p>Oração</p>
             </div>
 
             <div className="pagfoto">
-              <img src={foto} alt="Fotos" />
+              <img src={foto.src} alt="Fotos" />
               <p>Fotos</p>
             </div>
 
@@ -75,18 +77,18 @@ cd
           <div className="icones">
             <a className='InstagramLink' target='_blank' rel="stylesheet" href="https://www.instagram.com/adelparquealvoradai/">
               <div className="paginstagram">
-                <img src={instagram} alt="Instagram" />
+                <img src={instagram.src} alt="Instagram" />
                 <p>Instagram</p>
               </div>
             </a>
 
             <div className="paggrupo">
-              <img src={grupo} alt="PGI" />
+              <img src={grupo.src} alt="PGI" />
               <p>PGI</p>
             </div>
 
             <div className="pagagenda">
-              <img src={agenda} alt="Agenda" />
+              <img src={agenda.src} alt="Agenda" />
               <p>Agenda</p>
             </div>
 
