47a234b32d07b28492c04d200cc0471a107cf831
Aplicativo Adel <127167230+GuilhermeMat@users.noreply.github.com>
2024-05-19 01:57:35 -0300

    UPDATE-CMP

--- AppAdel1/src/app/ministry/page.jsx
+++ AppAdel1/src/app/ministry/page.jsx
@@ -1,53 +1,73 @@
 import { Box, Typography } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 
 export default function Ministry() {
   return (
     <Box className="pageContainer">
-      <Box>
-        <Typography
-          fontWeight={800}
-          variant="h2"
-          color="white"
-          fontFamily="revert"
-          textAlign="center"
-          padding="20px"
+      <Box 
+        sx={{overflow:"auto"}}
+        className="pageContainer"
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
         >
-          Ministérios
-        </Typography>
-      </Box>
-      <Box>
-        <Typography
-          fontWeight={900}
-          fontFamily="revert"
-          variant="h3"
-          color="white"
-          textAlign="center"
-        >
-          Louvor
-        </Typography>
-      </Box>
-      <Box>
-        <Typography
-          fontWeight={900}
-          fontFamily="revert"
-          variant="h3"
-          color="white"
-          textAlign="center"
-        >
-          Mídia
-        </Typography>
-      </Box>
-      <Box>
-        <Typography
-          fontWeight={900}
-          fontFamily="revert"
-          variant="h3"
-          color="white"
-          textAlign="center"
-        >
-          Kids
-        </Typography>
-      </Box>
+        <Box>
+            <Box style={{margin:"50px"}}>
+                <Typography color='#fff'>Ministério Madureira - Luziânia-GO</Typography>
+            </Box>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">KIDS</Typography>
+                  <Typography variant="body2" color="text.secondary" textAlign="justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card sx={{ maxWidth: 345, margin: "5px 0 0 0"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">MÍDIA</Typography>
+                  <Typography variant="body2" color="text.secondary" textAlign="justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card sx={{ maxWidth: 345, margin: "5px 0 25px 0"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">LOUVOR</Typography>
+                  <Typography variant="body2" color="text.secondary" textAlign="justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Box>
+        </Box>
     </Box>
   );
 }
