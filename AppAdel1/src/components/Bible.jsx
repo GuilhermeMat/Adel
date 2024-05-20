5ecc568ef6d0b189a13849e9a9fa26a55e089d74
Aplicativo Adel <127167230+GuilhermeMat@users.noreply.github.com>
2024-05-20 19:35:24 -0300

    BIBLE

--- AppAdel1/src/components/Bible.jsx
+++ AppAdel1/src/components/Bible.jsx
@@ -1,4 +1,22 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box } from "@mui/material";
+
+const axios = require('axios');
+
+// URL da API da Bíblia
+const apiUrl = 'https://bible-api.com/john%203:16?translation=almeida';
+
+// Função para fazer a solicitação à API da Bíblia
+async function getBibleVerse() {
+    try {
+        const response = await axios.get(apiUrl);
+        // Se a solicitação for bem-sucedida, você pode acessar os dados da resposta
+        console.log(response.data);
+    } catch (error) {
+        // Se houver algum erro na solicitação, você pode lidar com ele aqui
+        console.error('Erro ao obter versículo da Bíblia:', error);
+    }
+}
+
+// Chamar a função para obter um versículo da Bíblia
+getBibleVerse();
