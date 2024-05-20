925eafe59f7ccb8fd45525c25dc5cdf09270ad4d
Aplicativo Adel <127167230+GuilhermeMat@users.noreply.github.com>
2024-05-20 18:55:20 -0300

    NOTHING

--- AppAdel1/src/components/Bible.jsx
+++ AppAdel1/src/components/Bible.jsx
@@ -1,31 +1,4 @@
-'use client'
-import { authentication } from "@/auth";
-import { useLoadingContext } from "@/context/LoadingContext";
-import { Box, Typography } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import Loading from "./Loading";
-
-export default function Bible() {
-  const { isLoading, setGlobalLoading } = useLoadingContext();
-  const router = useRouter();
-
-  useEffect(() => {
-    const isAuthenticated = authentication();
-    if (isAuthenticated) {
-      localStorage.clear();
-      router.push(isAuthenticated);
-      return;
-    }
-    setGlobalLoading(false);
-  }, []);
-
-  if (isLoading) return <Loading />;
-  return (
-    <Box className="pageContainer">
-      <Typography variant="h2" color="white">
-        Página da Bíblia
-      </Typography>
-    </Box>
-  );
-}
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Box } from "@mui/material";
