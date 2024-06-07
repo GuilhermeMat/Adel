d35f8017bf0f5dd188c4de1c617232135b9d6307
Hugo Christopher <0.97oliveira@gmail.com>
2024-06-07 13:24:43 -0300

    adding img to images and pgi pages

--- AppAdel1/src/components/Ministry.jsx
+++ AppAdel1/src/components/Ministry.jsx
@@ -9,6 +9,10 @@ import { useRouter } from "next/navigation";
 import { useLoadingContext } from "@/context/LoadingContext";
 import Loading from "./Loading";
 import { authentication } from "@/auth";
+import Image from "next/image";
+import kidsimg from "../img/kids.png";
+import louvorimg from "../img/louvor.png";
+import midiaimg from "../img/midia.png";
 
 export default function Ministry() {
   const { isLoading, setGlobalLoading } = useLoadingContext();
@@ -33,65 +37,105 @@ export default function Ministry() {
         display="flex"
         alignItems="center"
         flexDirection="column"
->
-        <Box
-          sx={{overflowY:"auto",
-          height: "85vh"
-          }} 
-        >
-            <Box style={{margin:"50px"}}>
-                <Typography color='#fff'>Ministério Madureira - Luziânia-GO</Typography>
-            </Box>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">KIDS</Typography>
-                  <Typography variant="body2" color="text.secondary" textAlign="justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            <Card sx={{ maxWidth: 345, margin: "5px 0 0 0"}}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">MÍDIA</Typography>
-                  <Typography variant="body2" color="text.secondary" textAlign="justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            <Card sx={{ maxWidth: 345, margin: "5px 0 25px 0"}}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">LOUVOR</Typography>
-                  <Typography variant="body2" color="text.secondary" textAlign="justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam optio tenetur quia cum repudiandae eaque facilis autem nesciunt, ut qui. Pariatur voluptas totam incidunt ratione, sed obcaecati officia non blanditiis.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            </Box>
+      >
+        <Box sx={{ overflowY: "auto", height: "85vh" }}>
+          <Box style={{ margin: "50px" }}>
+            <Typography color="#fff">
+              Ministério Madureira - Luziânia-GO
+            </Typography>
+          </Box>
+          <Card sx={{ maxWidth: 345, margin:"0px 0 10px 0" }}>
+            <CardActionArea>
+              <Image
+                height="230"
+                width="345"
+                src={kidsimg.src}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  fontWeight={700}
+                  fontFamily="serif"
+                >
+                  KIDS
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  textAlign="justify"
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam optio tenetur quia cum repudiandae eaque facilis
+                  autem nesciunt, ut qui. Pariatur voluptas totam incidunt
+                  ratione, sed obcaecati officia non blanditiis.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card sx={{ maxWidth: 345, margin: "10px 0 10px 0" }}>
+            <CardActionArea>
+              <Image
+                height="260"
+                width="345"
+                src={midiaimg.src}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  fontWeight={700}
+                  fontFamily="serif"
+                >
+                  MÍDIA
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  textAlign="justify"
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam optio tenetur quia cum repudiandae eaque facilis
+                  autem nesciunt, ut qui. Pariatur voluptas totam incidunt
+                  ratione, sed obcaecati officia non blanditiis.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card sx={{ maxWidth: 345, margin: "10px 0 25px 0" }}>
+            <CardActionArea>
+              <Image
+                height="260"
+                width="345"
+                src={louvorimg.src}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  fontWeight={700}
+                  fontFamily="serif"
+                >
+                  LOUVOR
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  textAlign="justify"
+                >
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam optio tenetur quia cum repudiandae eaque facilis
+                  autem nesciunt, ut qui. Pariatur voluptas totam incidunt
+                  ratione, sed obcaecati officia non blanditiis.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Box>
       </Box>
+    </Box>
   );
 }
