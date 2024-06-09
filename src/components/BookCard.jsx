36cd5c702dd65d9f7aacdaa37c5622def2f1ee6e
Aplicativo Adel <127167230+GuilhermeMat@users.noreply.github.com>
2024-06-09 12:26:25 -0300

    UPDATE

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -24,7 +24,7 @@ export default function BookCard({ name, abbrev }) {
           alignItems: "center",
           borderRadius: "8px",
           border: "1px solid rgba(76, 172, 253, 0.8)",
-          margin: 1,
+          margin: 1
         }}
       >
         <Typography sx={{ color: "white", fontSize: "14px", fontWeight: 600 }}>
