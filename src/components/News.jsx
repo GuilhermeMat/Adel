f87e1d81d6b247b13a82ce9d0dc36587ab4923a1
Joao-Ortega <joao_ortega17@hotmail.com>
2024-09-11 14:19:00 -0300

    Creation of news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,31 +1,66 @@
-'use client'
+"use client";
 import { authentication } from "@/auth";
 import { useLoadingContext } from "@/context/LoadingContext";
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import Loading from "./Loading";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import NewsCreation from "./NewsCreation";
+import Image from "next/image";
 
 export default function News() {
-   const { isLoading, setGlobalLoading } = useLoadingContext()
-   const router = useRouter()
+  const [newsList, setNewsList] = useState([]);
+  const { isLoading, setGlobalLoading } = useLoadingContext();
+  const router = useRouter();
 
   useEffect(() => {
     const isAuthenticated = authentication();
-    if (isAuthenticated === '/') {
+    if (isAuthenticated === "/") {
       localStorage.clear();
       router.push(isAuthenticated);
       return;
     }
-    setGlobalLoading(false)
-  }, [])
+    setGlobalLoading(false);
+  }, []);
 
-  if (isLoading) return <Loading />
+  if (isLoading) return <Loading />;
 
-   return (
-      <Box>
-         <Typography variant='h2' color='white'>Página das Notícias</Typography>
-  
-      </Box>
-      ); 
+  return (
+    <Box
+      sx={{
+        height: "85vh",
+        overflowY: "auto",
+      }}
+    >
+      <NewsCreation newsList={newsList} addNews={setNewsList} />
+      <Divider sx={{ border: "1px solid grey" }} />
+      {newsList.length
+        ? newsList.map((news) => (
+            <Box
+              sx={{
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <Typography sx={{ color: "white", fontSize: '20px', fontWeight: 600 }}>{news.title}</Typography>
+              <Box
+                sx={{
+                  borderRadius: "8px",
+                  overflow: "hidden",
+                  width: 320,
+                  height: 250,
+                  margin: '1% 0'
+                }}
+              >
+                <Image width={320} height={250} src={news.src} />
+              </Box>
+              <Typography sx={{ color: "white", width: '300px', textAlign: 'justify'}}>
+                {news.description}
+              </Typography>
+            </Box>
+          ))
+        : null}
+    </Box>
+  );
 }
---
+++ src/components/NewsCreation.jsx
@@ -0,0 +1,148 @@
+import { Box, Button, Input, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+const NewsCreation = ({ newsList, addNews }) => {
+  const [fileName, setFileName] = useState();
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState();
+
+  const handleFileChange = (event) => {
+    const file = event.target.files && event.target.files[0];
+
+    if (file) {
+      setFileName(file.name);
+      const imageUrl = URL.createObjectURL(file);
+      setImg(imageUrl);
+    }
+  };
+
+  useEffect(() => {
+    if (img && title && description) {
+      setBtnDisable(false);
+    } else {
+      setBtnDisable(true);
+    }
+  }, [img, title, description]);
+
+  const handleNewsCreation = () => {
+    const newList = JSON.parse(JSON.stringify(newsList))
+    const newInfo = {
+      title,
+      src: img,
+      description,
+    }
+    newList.push(newInfo)
+    addNews(newList)
+    setImg("");
+    setTitle("");
+    setDescription("");
+    setFileName("");
+  };
+
+  return (
+    <Box
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "column",
+        height: img ? "260px" : "180px",
+      }}
+    >
+      <Input
+        sx={{
+          backgroundColor: "white",
+          borderRadius: "8px",
+          padding: 0.5,
+          width: "250px",
+        }}
+        placeholder="Título da Notícia"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {img && (
+          <Box
+            sx={{
+              borderRadius: "8px",
+              overflow: "hidden",
+              width: 140,
+              height: 100,
+              margin: "1% 2% 0 14%",
+            }}
+          >
+            <Image width={140} height={100} src={img} />
+          </Box>
+        )}
+        <Box
+          sx={{
+            margin: img ? "25px 0 0 0" : "0",
+          }}
+        >
+          <label htmlFor="file-upload">
+            <Button
+              variant="contained"
+              component="span"
+              sx={{
+                backgroundColor: "rgba(76, 172, 253, 0.8)",
+              }}
+            >
+              {fileName ? fileName : "Escolha imagem da Notícia"}
+            </Button>
+          </label>
+
+          {/* Input escondido */}
+          <Input
+            id="file-upload"
+            type="file"
+            onChange={handleFileChange}
+            sx={{ display: "none" }} // Escondendo o input padrão
+          />
+
+          {/* Exibindo o nome do arquivo após seleção */}
+          {fileName && (
+            <Typography variant="body2" sx={{ marginTop: 1 }}>
+              Arquivo Selecionado: {fileName}
+            </Typography>
+          )}
+        </Box>
+      </Box>
+      <Input
+        sx={{
+          backgroundColor: "white",
+          borderRadius: "8px",
+          padding: 0.5,
+          width: "250px",
+          height: "60px",
+          overflowY: "scroll",
+          wordWrap: "break-word",
+        }}
+        multiline
+        placeholder="Descrição..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <Button
+        variant="contained"
+        size="small"
+        disabled={btnDisable}
+        onClick={handleNewsCreation}
+        sx={{
+          backgroundColor: "rgba(76, 172, 253, 0.8)",
+        }}
+      >
+        Criar Notícia
+      </Button>
+    </Box>
+  );
+};
+
+export default NewsCreation;
